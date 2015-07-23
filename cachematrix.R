38d972fc6c538ecb050390d173b7677246e5849d
fabianobr <fabiano@freitassilva.com>
2015-07-22 23:09:18 -0300

    Solution for exercice, R programming, peer assessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The "makeCacheMatrix" function creates a special "matrix" which is really a list containing a set of functions to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverted matrix
+## 4. get the value of the inverted matrix  
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(inputMatrix = matrix()) {
+        cachedInvertedMatrix <- NULL
+        set <- function(y) {
+                inputMatrix <<- y
+                cachedInvertedMatrix <<- NULL
+        }
+        get <- function() inputMatrix
+        setCachedInvertedMatrix <- function(inverse) cachedInvertedMatrix <<- inverse
+        getCachedInvertedMatrix <- function() cachedInvertedMatrix
+        list(set = set, get = get,
+             setCachedInvertedMatrix = setCachedInvertedMatrix,
+             getCachedInvertedMatrix = getCachedInvertedMatrix)
 }
 
 
-## Write a short comment describing this function
+## The "cacheSolve" function calculates the "inverted matrix" (or solve function) of the special "matrix" created with the "makeCacheMatrix" function.
+## It first checks to see if the inverted matrix has already been calculated.
+## If so, it gets the inverted matrix from the cache and skips the computation.
+## Otherwise, it calculates the inverted matrix (solve function) of the data and sets the value of the inverted matrix in the cache via setCachedInvertedMatrix function. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invertedMatrix = x$getCachedInvertedMatrix()
+        
+        if (!is.null(invertedMatrix)) {
+                message("getting cached inverted matrix data")
+                return(invertedMatrix)
+        }
+        
+        input.data <- x$get()
+        invertedMatrix = solve(input.data, ...)
+        
+        x$setCachedInvertedMatrix(invertedMatrix)
+        
+        return(invertedMatrix)
 }
